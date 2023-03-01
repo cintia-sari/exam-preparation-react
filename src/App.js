6525fc7d208679a1bad58a5144421aaed5d4a205
Sári Cintia <sari.cintia1996@gmail.com>
2023-03-01 10:41:52 +0100

    construction of a  data structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const [state,setState]= React.useState(
+    {
+      group:[{
+        title : "Exam name",
+        GroupSetting: false,
+        allKnowledge:0,
+        GroupId : 0, 
+        itemsList:[{
+          name :"Topic name",
+          topicRename:false,
+          knowledge: 0,
+          topic: false,
+          topicId:0,
+            }  
+        ], 
+      }
+      ],
+      nextGrupId:1 ,
+      nextTopicId:1,
+    }
+  );
+
+
   return (
-    <div className="App">
-     
-    </div>
+    <>
+     <h1>Exam Preparation</h1>
+    </>
   );
 }
 
